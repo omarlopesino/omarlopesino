2cbefd087842bbf1c38b1599b177a377298b6892
Omar <omarmoper@gmail.com>
2026-06-24 21:10:41 +0200

    Automatic redirection to available languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,13 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ["en", {
+      path: "es",
+      codes: ["es", "es-ES", "es-MX"]
+    }],
+    defaultLocale: "en"
+  },
   vite: {
       plugins: [tailwindcss()],
     },

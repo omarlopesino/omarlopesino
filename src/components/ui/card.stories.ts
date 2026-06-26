cd70d4358ae5cb008c3bbe041f913f338d084f84
Omar <omarmoper@gmail.com>
2026-06-26 23:41:41 +0200

    IMprove card so it can have multiple variants

--- src/components/ui/card.stories.ts
+++ src/components/ui/card.stories.ts
@@ -1,21 +1,46 @@
 import Card from "./Card.astro";
 
 const image = '<img alt="Placeholder image" src="https://placehold.co/600x400/EEE/31343C" />'
-const cta = 
-  '<a class="btn btn-primary" href="#">Call to action</btn>';
+const content = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>';
+const actionClass = 'justify-end';
 
+const defaultSlots = {
+  content: content,
+  image: image,
+};
 
 export default {
   title: 'Components/Card',
   component: Card,
   args:  {
+    class: 'w-70',
     title: 'Card example',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    slots: {
-      image: image,
-      cta: cta,
-    },
+    slots: defaultSlots,
   },
 };
 
 export const Default = {};
+
+export const WithAction = {
+  args: {
+    actions: true,
+    actionClass: actionClass,
+    slots: {
+      ...defaultSlots,
+      cta: '<a class="btn btn-primary" href="#">Call to action</a>',
+    },
+  },
+};
+
+export const PostTeaser = {
+  args: {
+    class: 'w-70',
+    tag: 'article',
+    actionClass: actionClass,
+    url: '#',
+    slots: {
+      content: '<div class="flex gap-3 py-2"><time datetime="2026-06-26" class="text-sm">2026-06-26</time> <div class="flex gap-3"><span class="badge badge-accent">Tag 1</span><span class="badge badge-accent">Tag 2</span></div></div>' + content,
+      image: image,
+    },
+  },
+};

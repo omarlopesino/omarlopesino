a61a808b0a929336da2185e054c82b5d97f24938
Omar <omarmoper@gmail.com>
2026-06-28 21:03:03 +0200

    Improve searchbar

--- src/components/ui/searchbar.stories.ts
+++ src/components/ui/searchbar.stories.ts
@@ -3,6 +3,9 @@ import SearchBar from "./SearchBar.astro";
 export default {
   title: 'Components/SearchBar',
   component: SearchBar,
+  args: {
+    name: 'text'
+  }
 };
 
 export const Default = {};

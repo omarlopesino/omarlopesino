c81925eb340edcf54570b9849727a1ff1eca69e9
Omar <omarmoper@gmail.com>
2026-06-30 19:38:03 +0200

    Date interval component

---
+++ src/components/ui/DateInterval.stories.ts
@@ -0,0 +1,10 @@
+import DateInterval from "./DateInterval.astro";
+export default {
+  title: 'Components/DateInterval',
+  component: DateInterval,
+  args:  {
+    date_a: '2026-06',
+    date_b: '2026-07',
+  },
+};
+export const Default = {};

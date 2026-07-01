c5cf3764919e9d1f4efee76c9fe6d2b62b68ac10
Omar <omarmoper@gmail.com>
2026-07-01 21:51:42 +0200

    Utility to format date in post layout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function cn(...classes: (string | undefined | null | false)[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+export function formatDateYmd(date: Date) {
+    return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+}
